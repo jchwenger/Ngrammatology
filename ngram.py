7728eb6eb805b59cc9a3238c907fb07cb1690533
jchwenger <34098722+jchwenger@users.noreply.github.com>
2019-07-07 15:44:08 +0100

    adding helper function in ngram.py

---
+++ ngram.py
@@ -0,0 +1,38 @@
+corpora = {
+    'English': 15,
+    'American English': 17,
+    'French': 19,
+    'German': 20,
+    'Italian': 22
+}
+
+def ngram_url(kwords,
+              year_start='1900',
+              year_end='2008',
+              corpus='English'):
+    
+    params = { 
+        'year_start': year_start,
+        'year_end': year_end,
+        'corpus': corpus,
+        'kwords': kwords
+    }
+
+    content = '%2C'.join(params['kwords']).replace(' ', '%20')
+    direct_url = '.'.join(['t1%3B%2C' + kword.replace(' ', '%20') + '%3B%2Cc0' for kword in params['kwords']])
+    
+    ngram_string = "https://books.google.com/ngrams/interactive_chart?content={}&\
+                    year_start={}&\
+                    year_end={}&\
+                    corpus={}&\
+                    smoothing=3&\
+                    share=&\
+                    direct_url={}"\
+                        .format(content, 
+                                params['year_start'], 
+                                params['year_end'],
+                                params['corpus'],
+                                direct_url)\
+                        .replace(' ', '') # remove all white space
+
+    return ngram_string
